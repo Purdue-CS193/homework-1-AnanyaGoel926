e35ce4267e03abaf82e41ed001210ef124e2907a
Ananya Goel <goel56@data.cs.purdue.edu>
2021-09-15 22:54:20 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,42 +4,43 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt HW1.txt
 
 # Task 2
-
+rm -rf Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv fafsa.jpg Memes/SpongeBob
 
 # Task 4
-
+rm -rf  Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp C++
 
 # Task 7
-
+cp -R Programs/Java Homework/CS180 /Project_1
 
 # Task 8
-
+mv Programs/Count.c Programs/Goodbye.c Programs/C
 
 # Task 9
-
+mv Programs/Name.py Programs/Space.py "Python 3"
 
 # Task 10
-
+touch Homework/POL141/Essay.txt
 
 # Task 11
-
+ls ORIGIN
 
 # Task 12
 : '
 Type your vim/nano customizations below:
-
-
+:set number 
+:set showmatch
+:set colorscheme onedark
 
 '
